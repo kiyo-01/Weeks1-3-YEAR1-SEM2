60f53a5c65a486b73cdd8c9ceacdf0ea7a22a47f
Ryan Chen <cheryanj@shernet.sheridancollege.ca>
2026-01-05 11:17:11 -0500

    in-class work; screen to world and world to screen, primitive shape movement basics and mouse usage

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    float speed = 0.01f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = transform.position;
+        
+        newPosition.x += speed;
+
+        transform.position = newPosition;
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint (transform.position);
+
+        if (screenPosition.x >= Screen.width | screenPosition.x <= 0)
+        {
+            speed *= -1;
+        }
+    }
+}
